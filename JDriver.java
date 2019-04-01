cfe89321d1f2289a69702d7908d209f246b5bc6f
vliu00 <vliu00@stuy.edu>
2019-04-01 14:50:42 -0400

    adding other driver

---
+++ JDriver.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class JDriver {
+  public static void main(String[] args) {
+    int size = Integer.parseInt(args[0]);
+    int range = Integer.parseInt(args[1]);
+    Random r = new Random((int)System.currentTimeMillis());
+    int[] ary = new int[size];
+	int[] copy = new int[size];
+    for(int i = 0; i < ary.length; i++) {
+      ary[i] = Math.abs(r.nextInt());
+	  copy[i] = ary[i];
+    }
+	Arrays.sort(copy);
+	long x = System.currentTimeMillis();
+	Radix.radixsort(ary);
+	x = System.currentTimeMillis() - x;
+    System.out.println(x + " milliseconds");
+	System.out.println(Arrays.equals(ary,copy));
+
+  }
+
+
+}
